ca80df6a68a67f778b93eb681777ce75278e4a8a
Yasin Yavuz <barbaros1675@gmail.com>
2022-11-03 20:23:09 +0100

    icons and components added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
+import CardGrid from "../components/CardGrid";
+import TextFieldComp from "../components/TextFieldComp";
 import TypoButtons from "../components/TypoButtons";
 
 const Home = () => {
   return (
     <>
       <TypoButtons />
+      <TextFieldComp />
+      <CardGrid />
     </>
   );
 };
